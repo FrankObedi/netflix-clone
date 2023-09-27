ed54cca81c1d7f70da8cec46a0fe79e0b5898077
Frank Obedi <frankmobedi@gmail.com>
2023-09-27 17:39:24 -0400

    first commit

---
+++ app/auth/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+import Input from "@/components/input";
+import { useState, useCallback } from "react";
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant == "login" ? "register" : "login"
+    );
+  }, []);
+
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant == "login" ? "Sign In" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant == "register" && (
+                <Input
+                  id="name"
+                  label="Username"
+                  onChange={(e: any) => setUsername(e.target.value)}
+                  type="text"
+                  value={username}
+                />
+              )}
+              <Input
+                id="email"
+                label="Email"
+                onChange={(e: any) => setEmail(e.target.value)}
+                type="email"
+                value={email}
+              />
+              <Input
+                id="password"
+                label="Password"
+                onChange={(e: any) => setPassword(e.target.value)}
+                type="password"
+                value={password}
+              />
+            </div>
+            <button className="bg-red-600 py-2 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant == "login" ? "Login" : "Sign Up"}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant == "login"
+                ? "New to Netflix?"
+                : "Already have an account?"}
+
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:uderline cursor-pointer"
+              >
+                {variant == "login" ? "Sign up now" : "Login"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
